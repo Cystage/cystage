592b3deed5b3f394b885373afc75abe1f05e1483
Esteban <estebandle64@gmail.com>
2026-04-25 21:38:38 +0200

    fix css mobile

--- resources/js/pages/Unconnect.svelte
+++ resources/js/pages/Unconnect.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import AppHead from '@/components/AppHead.svelte';
     import logo from './img/logo.png';
+
+    let showModal = false;
 </script>
 
 <AppHead title="Bienvenu">
