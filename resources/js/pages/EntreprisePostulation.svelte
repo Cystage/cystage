3c35452988c3535c92ec1355aff6fd4b7bf6834b
BerretMan <ultimne@gmail.com>
2026-04-28 10:34:10 +0200

    delete fixe

--- resources/js/pages/EntreprisePostulation.svelte
+++ resources/js/pages/EntreprisePostulation.svelte
@@ -33,7 +33,7 @@
             <h3>Voir cv : <span style="color:blue"><a href="/storage/{p.path}">ici</a></span></h3>
             <center>
               <button class="close" onclick={() => console.log(p)}><p>✅</p></button>
-                <button class="close" onclick={() => supprimer(p.offre_id)}><p>❌</p></button>
+                <button class="close" onclick={() => supprimer(p.id)}><p>❌</p></button>
             </center>
         </div>
     {/each}
